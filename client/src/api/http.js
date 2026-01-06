0f406a120f8bccc16af1f3faec396eddf6458419
Aquadev-stack <aquadev010@gmail.com>
2026-01-06 04:02:29 -0900

    Configure CORS for Netlify + Render

--- client/src/api/http.js
+++ client/src/api/http.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API = import.meta.env.VITE_API_URL || "https://inioluwa-peace-cbt.onrender.com";
 
 export const http = axios.create({
   baseURL: `${API}/api`,
